e229de9c0c07b633652a24b0bd4fa91a67799ee7
Chanise Hart <niecy23@gmail.com>
2022-12-06 13:39:18 -0700

    Exercise 2 Attempt 1

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject?");
+            var favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+              case "math":
+                    Console.WriteLine("Math - Numbers are fun!");
+                    break;
+              case "writing":
+                    Console.WriteLine("Writing - Don't turn into a grammar police!");
+                    break;
+              case "history":
+                    Console.WriteLine("History - We're bound to repeat it if we don't learn it!");
+                    break;
+              case "science":
+                    Console.WriteLine("Science - Will you be the next Enstein?");
+                    break;
+              case "reading":
+                    Console.WriteLine("Reading - Everyone loves a good book!");
+                    break;
+              default:
+                    Console.WriteLine("Well that's a new one!");
+                    break;
+            }
         }
     }
 }
